7b7144437b94346941876d3f3ea82b11d1501ca7
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-15 12:52:44 +0200

    imported packages

---
+++ vue-header/src/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+
+<template>
+    <header>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+        </ul>
+    </header>
+</template>
+
+<style scoped></style>
