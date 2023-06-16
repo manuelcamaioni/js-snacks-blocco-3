6aac40b9f8c84870d0a3ec451ba33d3e3519a54b
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-16 12:27:42 +0200

    js-snack

---
+++ vue-header/src/components/AppHeader.vue
@@ -0,0 +1,52 @@
+<script>
+import NavLink from "./NavLink.vue";
+export default {
+    components: { NavLink },
+    data() {
+        return {
+            NavList: [
+                {
+                    name: "Home",
+                    link: "#",
+                },
+                {
+                    name: "Staff",
+                    link: "#",
+                },
+                {
+                    name: "Lavora con noi",
+                    link: "#",
+                },
+                {
+                    name: "Info",
+                    link: "#",
+                },
+                {
+                    name: "Contattaci",
+                    link: "#",
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+        <ul>
+            <li v-for="element in NavList">
+                <NavLink :link="element.link">{{ element.name }}</NavLink>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+ul {
+    list-style: none;
+
+    li {
+        margin-right: 1rem;
+    }
+}
+</style>
---
+++ vue-header/src/components/NavLink.vue
@@ -0,0 +1,16 @@
+<template>
+    <a href="link"></a>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+    props: {
+        link: "String",
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
